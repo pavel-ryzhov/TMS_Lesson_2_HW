1e4ca088ae4556c0b5bddc82457c692f15ff898e
pavel-ryzhov <pavelryzhov06@gmail.com>
2021-08-18 16:59:41 +0300

    Initial commit

---
+++ src/N6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class N6 {
+    /**
+     * В методе main получить введенное с консоли слово и вывести на консоль выражение: введенное слово "и_тут_в_кавычках_должно_быть_введенное_слово".
+     */
+    public static void main(String[] args) {
+        System.out.printf("введенное слово \"%s\"", new Scanner(System.in).nextLine());
+    }
+}
